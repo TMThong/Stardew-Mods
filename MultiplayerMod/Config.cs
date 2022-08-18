5fe67156fbfd6c1f4d9cd5e387150675821936c7
mthongdev@gmail.com <mthongdev@gmail.com>
2022-08-18 13:51:53 +0700

    Add project files.

---
+++ MultiplayerMod/Config.cs
@@ -0,0 +1,14 @@
+﻿using StardewModdingAPI;
+
+namespace MultiplayerMod
+{
+    internal class Config
+    {
+        public SButton debugKey { get; set; }
+
+        public Config()
+        {
+            debugKey = SButton.J;
+        }
+    }
+}
