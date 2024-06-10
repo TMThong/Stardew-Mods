6ffeb8298e2abf0bfef0c869199298a938e99235
Trần Minh Thông <98743684+TMThong@users.noreply.github.com>
2024-06-10 13:05:39 +0700

    add new mods

---
+++ CloudSave/Config.cs
@@ -0,0 +1,14 @@
+﻿using StardewModdingAPI;
+
+namespace CloudSave
+{
+    internal class Config
+    {
+        public SButton debugKey { get; set; }
+
+        public Config()
+        {
+            debugKey = SButton.J;
+        }
+    }
+}
