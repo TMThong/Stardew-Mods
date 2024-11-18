b3a7ab46097e7c460fcc09d1b014730954a99f18
Trần Minh Thông <mthongdev@gmail.com>
2024-11-18 14:01:22 +0700

    Create Config.cs

---
+++ Ping/Config.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ping
+{
+    public class Config
+    {
+        public Color PingFast { get; set; } = Color.Green;
+
+        public Color PingNormal { get; set; } = Color.Yellow;
+
+        public Color PingWeak { get; set; } = Color.Orange;
+
+        public Color PingVeryWeak { get; set; } = Color.Red;
+    }
+}
