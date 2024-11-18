cc0cd3e33ee40cb9ed0629f72d9bdc66c6e31222
Trần Minh Thông <mthongdev@gmail.com>
2024-11-18 14:05:34 +0700

    Update Config.cs

--- Ping/Config.cs
+++ Ping/Config.cs
@@ -9,12 +9,21 @@ namespace Ping
 {
     public class Config
     {
+        /// <summary>
+        /// For pings between 0 and 50 ms
+        /// </summary>
         public Color PingFast { get; set; } = Color.Green;
-
+        /// <summary>
+        /// For pings between 51 and 150 ms
+        /// </summary>
         public Color PingNormal { get; set; } = Color.Yellow;
-
+        /// <summary>
+        /// For pings between 151 and 300 ms
+        /// </summary>
         public Color PingWeak { get; set; } = Color.Orange;
-
+        /// <summary>
+        /// For pings greater than 301 ms
+        /// </summary>
         public Color PingVeryWeak { get; set; } = Color.Red;
     }
 }
